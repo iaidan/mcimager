b1842090fb43c3375cbdd5eae7f382dda7dbc927
Aidan Taylor <me@aidantaylor.net>
2014-01-08 23:41:38 +0000

    Added Image Cache

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+$cache = true;//should the script cache images?
+
 if (isset($_REQUEST['url']) && !empty($_REQUEST['url'])) {//get url (passed from apache)
 	$url = explode("/", $_REQUEST['url']);
 	
@@ -15,38 +17,76 @@ if (isset($_REQUEST['url']) && !empty($_REQUEST['url'])) {//get url (passed from
 	die("Access Denied.");//no vairables found in url, deny access
 }
 
+//image cache locations
+$mcSkinCache = "cache/skin/{$mc}-{$size}.png";
+$mcHeadCache = "cache/head/{$mc}-{$size}.png";
+$mcHelmCache = "cache/helm/{$mc}-{$size}.png";
+
+//location to store last cache date
+$dateCache   = "tmp/{$mc}-{$size}.cache.txt";
+
 header("Content-Type: image/png");//make output png
 
-$skinUrl = "http://www.minecraft.net/skin/{$mc}.png";//minecraft skin url
-
-if (!@file_get_contents($skinUrl)) {//check for skins existance
-	$skinUrl = "http://www.minecraft.net/skin/char.png";//no skin exists, return char (Steve)
+//check last cache time
+if ($data = @file_get_contents($dateCache)) {
+    $mysqldate = strtotime(date('Y-m-d H:i:s', $data));
+} else {
+    $mysqldate = false;
 }
 
-$skin = @imagecreatefrompng($skinUrl);//create skin image
+$mysqldateToday = strtotime(date('Y-m-d H:i:s'));
 
-$head = imagecreatetruecolor($size, $size);//create blank canvas
-imagecopyresampled($head, $skin, 0, 0, 8, 8, $size, $size, 8, 8);//copy head onto canvas
-
-$helm = imagecreatetruecolor($size, $size);//create blank canvas
-imagesavealpha($helm, true);//set alpha
-imagealphablending($helm, false);//remove alpha blending
-
-imagecopyresampled($helm, $skin, 0, 0, 40, 8, $size, $size, 8, 8);//move helm onto canvas
+if ($cache === false || $mysqldate === false || ($mysqldateToday - $mysqldate) >= 600) {
+	$skinUrl = "http://www.minecraft.net/skin/{$mc}.png";
+	
+	$skinGrab = @file_get_contents($skinUrl);
+	
+	if (!$skinGrab) {
+		$skinUrl = "http://www.minecraft.net/skin/char.png";//no skin exists, return char (Steve)
+		$skinGrab = @file_get_contents($skinUrl);
+	}
+	
+	$skinImage = imagecreatefromstring($skinGrab);//load skin image
+	file_put_contents($mcSkinCache, $skinGrab);//save skin
+	
+	//head
+	$headImage = imagecreatetruecolor($size, $size);//create blank canvas
+	imagecopyresampled($headImage, $skinImage, 0, 0, 8, 8, $size, $size, 8, 8);//copy head onto canvas
+	imagepng($headImage, $mcHeadCache);//save head
+		
+	//helm
+	$tempImage = imagecreatetruecolor($size, $size);//create blank canvas
+	$helmImage = imagecreatefrompng($mcHeadCache);//load head
+	imagesavealpha($tempImage, true);//set alpha
+	imagealphablending($tempImage, false);//remove alpha blending
+	
+	imagecopyresampled($tempImage, $skinImage, 0, 0, 40, 8, $size, $size, 8, 8);//move helm onto canvas
+	imagecopy($helmImage, $tempImage, 0, 0, 0, 0, $size, $size);//move helm onto head
+	imagepng($helmImage, $mcHelmCache);//save head
+    
+	//save cache date file
+    $fh = fopen($dateCache, 'w');
+    fwrite($fh, $mysqldateToday);
+    fclose($fh);
+} else {
+	$skinImage = imagecreatefrompng($mcSkinCache);
+	$helmImage = imagecreatefrompng($mcHelmCache);
+	$headImage = imagecreatefrompng($mcHeadCache);
+}
 
 switch($url[0]) {//switch function
 	case "head":
-		imagepng($head);//display head
+		imagepng($headImage);//display head
 		break;
 	case "helm":
-		imagecopy($head, $helm, 0, 0, 0, 0, $size, $size);//move helm onto head
-		imagepng($head);//display head
+		imagepng($helmImage);//display head
 		break;
 	case "skin":
-		imagepng($skin);//display skin
+		imagepng($skinImage);//display skin
 		break;
 }
-
-imagedestroy($skin);//destroy skin
-imagedestroy($head);//destroy head
-imagedestroy($helm);//destroy helm
+	
+//destroy images
+@imagedestroy($skinImage);
+@imagedestroy($headImage);
+@imagedestroy($helmImage);
